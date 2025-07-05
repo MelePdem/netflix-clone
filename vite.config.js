0c728ae8cf746899331103b79e1a16e47af1f9a0
MelePdem <melesedemessie88@gmail.com>
2025-07-05 18:28:59 -0400

    new update

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/netflix-clone/",
-// });
-
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: "/", // ✅ Important for Vercel
-})
+  base: "/netflix-clone/",
+});
