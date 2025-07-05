f61517b7fe042e53fea25af7651a23d9f45a0932
MelePdem <melesedemessie88@gmail.com>
2025-07-05 18:49:22 -0400

    � set base to / for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: "/",    // ← root‑relative paths for all built assets
+});
 
-// export default defineConfig({
-//   plugins: [react()],
-//   base: "/netflix-clone/",
-// });
